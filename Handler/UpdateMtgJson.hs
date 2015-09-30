4276661ef2712bdefd46d40d23a4464958fd7cea
Spencer Janssen <spencerjanssen@gmail.com>
2015-09-29 21:12:46 -0500

    Add Card to DB and an mtgjson importer

---
+++ Handler/UpdateMtgJson.hs
@@ -0,0 +1,42 @@
+-- IMPORTANT TODO: restrict access to this page to admins only
+module Handler.UpdateMtgJson where
+
+import Import
+import Data.Aeson
+import qualified Data.Map as Map
+
+getUpdateMtgJsonR :: Handler Html
+getUpdateMtgJsonR = do
+    defaultLayout
+        [whamlet|
+            <form method=post action=@{UpdateMtgJsonR}>
+                <button .btn btn-primary type="submit">
+                    Reload mtgjson.com data
+        |]
+
+data CardInfo = CardInfo { cicolors :: [Text], citypes :: [Text] }
+
+instance FromJSON CardInfo where
+    parseJSON (Object v) = CardInfo
+                            <$> v .:? "colors" .!= []
+                            <*> v .:? "types" .!= []
+    parseJSON _ = mzero
+
+massage :: Map Text CardInfo -> [Card]
+massage = map (\(n, i) -> Card n (cicolors i) (citypes i)) . Map.toList
+
+postUpdateMtgJsonR :: Handler Html
+postUpdateMtgJsonR = do
+    req <- parseUrl url
+    js <- responseBody <$> httpLbs req
+    case massage <$> eitherDecode js of
+        Left err -> fail err
+        Right cs -> do
+            runDB $ do
+                deleteWhere ([] :: [Filter Card])
+                mapM_ (\c -> insert c >> return ()) cs
+            defaultLayout [whamlet|
+                <p>Successfully loaded #{length cs} cards
+            |]
+ where
+    url = "http://mtgjson.com/json/AllCards.json"
---
+++ test/Handler/UpdateMtgJsonSpec.hs
@@ -0,0 +1,14 @@
+module Handler.UpdateMtgJsonSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getUpdateMtgJsonR" $ do
+        error "Spec not implemented: getUpdateMtgJsonR"
+
+
+    describe "postUpdateMtgJsonR" $ do
+        error "Spec not implemented: postUpdateMtgJsonR"
+
