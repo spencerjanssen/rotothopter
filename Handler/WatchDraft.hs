4a140e98e0353d53506e12124d290b4651c0b2f5
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-04 20:12:13 -0500

    EventSource fixes
    Log an initial event right away which hopefully keeps nginx from closing
    the connection.  Also add some debugging to the eventsource javascript.

--- Handler/WatchDraft.hs
+++ Handler/WatchDraft.hs
@@ -8,10 +8,12 @@ import Network.Wai.EventSource
 import Data.ByteString.Builder
 
 getWatchDraftR :: DraftId -> Handler TypedContent
-getWatchDraftR draftId = do
-    tc <- subscribeDraftWatcher draftId
-    pollingEventSource () $ \_ () -> do
-        dp <- atomically $ readTChan tc
-        return ([ServerEvent mesg_name Nothing [intDec $ draftPickPickNumber dp]], ())
+getWatchDraftR draftId = pollingEventSource Nothing pollFn
  where
     mesg_name = Just "observe_pick"
+    pollFn _ Nothing = do
+        tc <- subscribeDraftWatcher draftId
+        return ([ServerEvent (Just "initialized") Nothing ["initialized"]], Just tc);
+    pollFn _ (Just tc) = do
+        dp <- atomically $ readTChan tc
+        return ([ServerEvent mesg_name Nothing [intDec $ draftPickPickNumber dp]], Just tc)
