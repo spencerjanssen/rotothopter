f64ba6a22f77b72e82979c9e5d1aa01e14a75734
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-07 01:38:41 -0500

    HomeSpec now passes tests

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -7,20 +7,12 @@ spec = withApp $ do
     it "loads the index and checks it looks right" $ do
         get HomeR
         statusIs 200
-        htmlAllContain "h1" "Welcome to Yesod"
+        htmlAllContain "h1" "Welcome to rotothopter"
 
-        request $ do
-            setMethod "POST"
-            setUrl HomeR
-            addToken
-            fileByLabel "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
-            byLabel "What's on the file?" "Some Content"
+        -- also test some navbar stuff
+        htmlAllContain "ul.nav > li.active" "Home"
 
         statusIs 200
-        -- more debugging printBody
-        htmlCount ".message" 1
-        htmlAllContain ".message" "Some Content"
-        htmlAllContain ".message" "text/plain"
 
     -- This is a simple example of using a database access in a test.  The
     -- test will succeed for a fresh scaffolded site with an empty database,
