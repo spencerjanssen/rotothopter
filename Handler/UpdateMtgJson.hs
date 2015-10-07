46d8de63370f3801f2bb01756399402b5e123ae8
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-06 21:45:44 -0500

    Fix split cards and other multi name cards

--- Handler/UpdateMtgJson.hs
+++ Handler/UpdateMtgJson.hs
@@ -3,6 +3,7 @@ module Handler.UpdateMtgJson where
 import Import
 import Data.Aeson
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 getUpdateMtgJsonR :: Handler Html
 getUpdateMtgJsonR = do
@@ -13,16 +14,62 @@ getUpdateMtgJsonR = do
                     Reload mtgjson.com data
         |]
 
-data CardInfo = CardInfo { cicolors :: [Text], citypes :: [Text] }
+-- Possible values: normal, split, flip, double-faced, token, plane, scheme, phenomenon, leveler, vanguard
+data Layout = Split
+            | Flip
+            | DoubleFaced
+            | MehLayout
+ deriving Show
+
+data CardInfo = CardInfo { ciname :: Text, cicolors :: [Text], citypes :: [Text], layout :: Layout, names :: Maybe [Text] }
+ deriving Show
 
 instance FromJSON CardInfo where
     parseJSON (Object v) = CardInfo
-                            <$> v .:? "colors" .!= []
+                            <$> v .: "name"
+                            <*> v .:? "colors" .!= []
                             <*> v .:? "types" .!= []
+                            <*> v .: "layout"
+                            <*> v .:? "names" .!= Nothing
     parseJSON _ = mzero
 
+instance FromJSON Layout where
+    parseJSON (String s) = return $
+                            case s of
+                                "split" -> Split
+                                "flip" -> Flip
+                                "double-faced" -> DoubleFaced
+                                _ -> MehLayout
+    parseJSON _ = mzero
+
+uniq :: Ord a => [a] -> [a]
+uniq xs = Set.toList . Set.fromList $ xs
+
+-- | Assume all CardInfo are part of the same split card
+combineCardInfo :: [CardInfo] -> CardInfo
+combineCardInfo xs@(CardInfo {layout = Split, names = Just ns}:_) = CardInfo
+    { ciname = name
+    , cicolors = uniq $ concatMap cicolors xs
+    , citypes = uniq $ concatMap citypes xs
+    , layout = Split
+    , names = Just ns
+    }
+ where
+    name = intercalate " // " ns
+combineCardInfo xs@(CardInfo {names = Just (n:_)}:_) = prime
+ where (prime:_) = [x | x <- xs, ciname x == n]
+combineCardInfo xs = error $ "called with bad argument: " ++ show xs
+
 massage :: Map Text CardInfo -> [Card]
-massage = map (\(n, i) -> Card n (cicolors i) (citypes i)) . Map.toList
+massage cs' = [Card (ciname ci) (cicolors ci) (citypes ci)
+                | ci <- singlenames ++ massagedMultinames]
+ where
+    cs = Map.elems cs'
+    singlenames = [c | c@(CardInfo {names = Nothing}) <- cs]
+    multinames = Map.elems $ Map.fromListWith (++)
+                    [(ns, [c]) | c@(CardInfo {names = Just ns}) <- cs ]
+    massagedMultinames = map combineCardInfo multinames
+-- map (\(n, i) -> Card n (cicolors i) (citypes i)) . Map.toList
 
 postUpdateMtgJsonR :: Handler Html
 postUpdateMtgJsonR = do
