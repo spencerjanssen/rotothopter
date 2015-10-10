48651d5f3e27cd270f0f949f9a8493d5e25894f7
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-09 22:20:10 -0500

    CubeList tests

--- test/Handler/CubeListSpec.hs
+++ test/Handler/CubeListSpec.hs
@@ -4,11 +4,33 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
+    describe "getNewCubeListR" $ do
+        it "loads the page" $ do
+            get NewCubeListR
+            statusIs 200
 
-    describe "getCubeListR" $ do
-        error "Spec not implemented: getCubeListR"
+    describe "postNewCubeListR" $ do
+        it "inserts the new cube in the DB" $ do
+            postTestCube
+            statusIs 303
+            Just (Entity _ cl) <- runDB $ getBy (UniqueCubeName testCubeName)
+            assertEqual "cube list" (cubeCubeList cl) testCubeList
 
+    describe "getViewCubeListR" $ do
+        it "displays the cube" $ do
+            postTestCube
+            Just (Entity id _) <- runDB $ getBy $ UniqueCubeName testCubeName
+            get $ ViewCubeListR id
+            statusIs 200
 
-    describe "postCubeListR" $ do
-        error "Spec not implemented: postCubeListR"
+testCubeName = "Test Cube"
+testCubeList = ["Life", "Death" , "Life // Death", "Lightning Bolt"]
 
+postTestCube = do
+    get NewCubeListR
+    request $ do
+        addToken
+        byLabel "Cube name" testCubeName
+        byLabel "The cube list" (unlines testCubeList)
+        setMethod "POST"
+        setUrl NewCubeListR
