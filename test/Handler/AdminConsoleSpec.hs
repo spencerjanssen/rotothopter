c2d3dfaa42087f99c686fefb357033cc7d1e9cd7
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-09 20:50:49 -0500

    Remove completed todo

--- test/Handler/AdminConsoleSpec.hs
+++ test/Handler/AdminConsoleSpec.hs
@@ -5,6 +5,3 @@ import TestImport
 spec :: Spec
 spec = withApp $ do
     describe "getAdminConsoleR" $ checkRequiresAdmin AdminConsoleR
-
-        -- TODO figure out how to authenticate as admin/non-admin and check
-        -- those cases too
