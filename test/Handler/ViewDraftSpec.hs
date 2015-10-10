8540b0ec6ca54ba105e5a2746169d3505dd85d90
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-10 17:17:08 -0500

    Implement ViewDraftSpec

--- test/Handler/ViewDraftSpec.hs
+++ test/Handler/ViewDraftSpec.hs
@@ -4,7 +4,13 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
-
     describe "getViewDraftR" $ do
-        error "Spec not implemented: getViewDraftR"
-
+        it "displays a draft" $ do
+            postCube testLargeCubeName testLargeCube
+            postDraft testLargeCubeName testParticipants testDraftRounds
+            did <- getOnlyDraftId
+            get $ ViewDraftR did
+            statusIs 200
+            htmlCount "th" (length testParticipants + 2)
+    -- TODO better tests that verify display of the draft table, available
+    -- cards and which participant is ready to draft
