1518d3fcc46afeed4996a893233de203545c5f4e
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-10 15:41:45 -0500

    Implement UpdateMtgJsonSpec

--- test/Handler/UpdateMtgJsonSpec.hs
+++ test/Handler/UpdateMtgJsonSpec.hs
@@ -6,9 +6,12 @@ spec :: Spec
 spec = withApp $ do
 
     describe "getUpdateMtgJsonR" $ do
-        error "Spec not implemented: getUpdateMtgJsonR"
+        checkRequiresAdmin UpdateMtgJsonR
 
-
+    {-
+     - TODO, figure out how to intercept the HTTP request this handler makes to
+     - insert a fake/abridged mtgjson document.  Downloading the entire 10 MB
+     - file is both expensive and nondeterministic.
     describe "postUpdateMtgJsonR" $ do
         error "Spec not implemented: postUpdateMtgJsonR"
-
+    -}
