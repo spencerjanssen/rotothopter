f73cbd663207820e6f286d2bb9f3f85d941a989e
Spencer Janssen <spencerjanssen@gmail.com>
2015-10-09 20:48:30 -0500

    Add test for AdminAddUser

--- test/Handler/AdminAddUserSpec.hs
+++ test/Handler/AdminAddUserSpec.hs
@@ -4,11 +4,23 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
-
     describe "getAdminAddUserR" $ do
-        error "Spec not implemented: getAdminAddUserR"
-
+        checkRequiresAdmin AdminAddUserR
 
     describe "postAdminAddUserR" $ do
-        error "Spec not implemented: postAdminAddUserR"
-
+        it "allows admin to add a user" $ do
+            authenticateAdmin
+            get AdminAddUserR
+            statusIs 200
+            request $ do
+                addToken
+                byLabel "Email" x
+                byLabel "Name" xname
+                setMethod "POST"
+                setUrl AdminAddUserR
+            statusIs 303
+            Just (Entity _ usr) <- runDB $ getBy $ UniqueUser x
+            assertEqual "userIdent" x (userIdent usr)
+            assertEqual "userDisplayName" (Just xname) (userDisplayName usr)
+ where
+    (x, xname) = ("x@test.com", "Xavier")
