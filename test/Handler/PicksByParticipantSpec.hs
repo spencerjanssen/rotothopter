bccc7d64cc06fe0e8251a74e990f1864b8e2840a
Spencer Janssen <spencerjanssen@gmail.com>
2015-11-06 14:06:40 -0600

    Picks by drafter: add forgotten files and tests

---
+++ test/Handler/PicksByParticipantSpec.hs
@@ -0,0 +1,20 @@
+module Handler.PicksByParticipantSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getPicksByParticipantR" $ do
+        it "loads the page" $ do
+            postCube testLargeCubeName testLargeCube
+            postDraft testLargeCubeName testParticipants testDraftRounds
+            did <- getOnlyDraftId
+            uid <- getFirstDrafter did
+            -- ugly...
+            get $ PicksByParticipantR did $ UserKey 1
+            statusIs 200
+
+getFirstDrafter did = do
+    Just (Entity _ x) <- runDB $ selectFirst [DraftParticipantDraft ==. did] []
+    return $ x ^. draftParticipantDrafter
