8c96206841cceeb717eb1f17a490155a09152ac1
Spencer Janssen <spencerjanssen@gmail.com>
2015-11-13 01:23:13 -0600

    Lint police

--- Application.hs
+++ Application.hs
@@ -56,8 +56,6 @@ import Handler.JoinDraftInvite
 import Handler.LaunchDraftInvite
 import Handler.PicksByParticipant
 
-import Common
-
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
