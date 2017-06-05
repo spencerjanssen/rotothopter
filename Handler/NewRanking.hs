b964b29f8c121bbd87252e387dff56ebb821ca2c
Spencer Janssen <spencerjanssen@gmail.com>
2017-06-05 00:05:39 -0500

    Redirect to ranking if one already exists

--- Handler/NewRanking.hs
+++ Handler/NewRanking.hs
@@ -5,5 +5,9 @@ import Import
 getNewRankingR :: CubeId -> Handler Html
 getNewRankingR cubeId = do
     uid <- requireAuthId
-    rid <- runDB $ insert $ Ranking uid cubeId
+    rid <- runDB $ do
+        mrid <- getBy $ UniqueRanking uid cubeId
+        case mrid of
+            Nothing -> insert $ Ranking uid cubeId
+            Just (Entity rid _) -> return rid
     redirect (ViewRankingR rid)
